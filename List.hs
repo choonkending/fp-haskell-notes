cb1b4e79f5e88490b601dbe767ea9f0779f61b72
Ken Ding <aenkil@gmail.com>
2018-02-02 09:00:14 +1100

    Add initial List commit

---
+++ List.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+-- Do not load Prelude when compiling a Haskell module
+
+module List where
+--  A module in Haskell:
+--  1. Controls namespaces
+--  2. Creates abstract data types
+
+data List a =
+  Nil
+  | Cons a (List a)
+
+
