0315c7a1cdc261c7d357cf121857322ff094291c
Ken Ding <aenkil@gmail.com>
2018-02-06 23:28:12 +1100

    Add foldLeft and foldLeft'

--- List.hs
+++ List.hs
@@ -6,7 +6,7 @@ module List where
 --  1. Controls namespaces
 --  2. Creates abstract data types
 
-import Prelude(Show, Integer, (+))
+import Prelude(Show, Integer, (+), seq)
 
 data List a =
   Nil
@@ -25,10 +25,11 @@ infinity =
 
 -- Common functions used over a List
 
+-- if the list is empty, the result is the initial value z; else
+-- apply f to the first element and the result of folding the rest
 foldRight :: (a -> b -> b) -> b -> List a -> b
 foldRight _ b Nil       = b
 foldRight f b (h :- t)  = f h (foldRight f b t)
-
 --
 -- list1 = 1 :- 2 :- 3 :- Nil
 --   foldRight (+) 0 list1
@@ -39,4 +40,16 @@ foldRight f b (h :- t)  = f h (foldRight f b t)
 -- = (+) 1 ((+) 2 ((+) 3 (+) 0)))
 -- = (+) 1 ((+) 2 (+) 3)
 -- = 6
+
+
+-- if the list is empty, the result is the initial value z; else
+-- we recursive immediately, making the new initial value the result
+-- of combining the old initial value with the first element
+foldLeft :: (b -> a -> b) -> b -> List a -> b
+foldLeft _ b Nil        = b
+foldLeft f b (h :- t)   = foldLeft f (f b h) t
+
+foldLeft' :: (b -> a -> b) -> b -> List a -> b
+foldLeft' _ b Nil       = b
+foldLeft' f b (h :- t)  = let b' = f b h in seq b' foldLeft' f b' t
 
